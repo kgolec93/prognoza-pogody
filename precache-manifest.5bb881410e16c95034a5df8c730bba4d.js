cf8c564665d22c738fd75a2dfc95f18d2c598ecb
kgolec93 <kamil.golec@gmail.com>
2019-08-26 22:24:39 +0200

    Updates

---
+++ precache-manifest.5bb881410e16c95034a5df8c730bba4d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a332a6700a96f70e1346f790a45e02",
+    "url": "/pogoda/static/media/clouds.91a332a6.jpg"
+  },
+  {
+    "revision": "fe700e026f9884196c47",
+    "url": "/pogoda/static/css/main.0ba486d0.chunk.css"
+  },
+  {
+    "revision": "fbb9667ca9527970824f",
+    "url": "/pogoda/static/js/runtime~main.fbb9667c.js"
+  },
+  {
+    "revision": "17e932fae7542485296d",
+    "url": "/pogoda/static/js/2.17e932fa.chunk.js"
+  },
+  {
+    "revision": "2cdeec72d321131581280d0e0c7db418",
+    "url": "/pogoda/static/media/sun.2cdeec72.svg"
+  },
+  {
+    "revision": "95bde7d41a60343e8047bb485064f707",
+    "url": "/pogoda/static/media/cloud.95bde7d4.svg"
+  },
+  {
+    "revision": "cb7ac759d10e529e67e0f377849374dd",
+    "url": "/pogoda/static/media/rain.cb7ac759.svg"
+  },
+  {
+    "revision": "f93f841cffceb8e536f180b108ed9ae0",
+    "url": "/pogoda/static/media/storm.f93f841c.svg"
+  },
+  {
+    "revision": "aaa9ea6875fae22e2926a5c3573bea08",
+    "url": "/pogoda/static/media/drizzle.aaa9ea68.svg"
+  },
+  {
+    "revision": "427ef95c6c2a6d5ea294ea83711191d4",
+    "url": "/pogoda/static/media/snow.427ef95c.svg"
+  },
+  {
+    "revision": "b998c21a9433049de97ad54ea8fde231",
+    "url": "/pogoda/static/media/mist.b998c21a.svg"
+  },
+  {
+    "revision": "cf697ba7a271e5bb64bcb157c580c29b",
+    "url": "/pogoda/static/media/sun.cf697ba7.jpg"
+  },
+  {
+    "revision": "fe700e026f9884196c47",
+    "url": "/pogoda/static/js/main.fe700e02.chunk.js"
+  },
+  {
+    "revision": "257d318aab0aeefa1d8e0b6b792a8f91",
+    "url": "/pogoda/static/media/rain.257d318a.jpg"
+  },
+  {
+    "revision": "031c8036599c814e58c09335f4ea5ff1",
+    "url": "/pogoda/static/media/storm.031c8036.jpg"
+  },
+  {
+    "revision": "dbe84497185bc6c3c224db35390381b9",
+    "url": "/pogoda/static/media/drizzle.dbe84497.jpg"
+  },
+  {
+    "revision": "51bed3a41a8ee40034fa10a4e7b61c3e",
+    "url": "/pogoda/static/media/snow.51bed3a4.jpg"
+  },
+  {
+    "revision": "1f2c22c705aa0e28682db44533f178e1",
+    "url": "/pogoda/static/media/mist.1f2c22c7.jpg"
+  },
+  {
+    "revision": "bfc53b6a6bee1a0b135dddd79dd8468d",
+    "url": "/pogoda/static/media/drop-down.bfc53b6a.svg"
+  },
+  {
+    "revision": "85fc7430c6a4d600aee64dc5dc2d8066",
+    "url": "/pogoda/static/media/arr-north.85fc7430.svg"
+  },
+  {
+    "revision": "290174b6259ea557de7fde682836d3c6",
+    "url": "/pogoda/static/media/arr-back.290174b6.svg"
+  },
+  {
+    "revision": "1ee4c3ee10a89d4ebe3782b2ff0d9eea",
+    "url": "/pogoda/static/media/arr-next.1ee4c3ee.svg"
+  },
+  {
+    "revision": "d9d208a8fdf6b1d9faf33baae414944f",
+    "url": "/pogoda/static/media/location.d9d208a8.svg"
+  },
+  {
+    "revision": "d9d8c3fd3aaf3e9510926a5b80bcec1a",
+    "url": "/pogoda/index.html"
+  }
+];
